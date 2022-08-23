c7860e9ec3d448757eb1cb3457b8f5ea134dba77
UosakiMasahide <uosakimasahide602@arsaga.jp>
2022-08-23 23:35:17 +0900

    install composition-api

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,6 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+require('./bootstrap')
+import { createApp } from 'vue/dist/vue.esm-bundler.js'
+import VueCompositionApi from '@vue/composition-api'
 
-import auth from './auth'
-import error from './error'
-import { HTTP_UNPROCESSABLE_ENTITY } from './axios/constants'
-
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
-    modules: {
-        auth,
-        error,
-    },
-})
-
-import httpClient from './axios'
-
-//共通レスポンス処理
-httpClient.interceptors.response.use(
-    (response) => {
-        response.success = true
-        return response
-    },
-    (error) => {
-        //厳密には、error.responseが取れない場合の処理が必要です
-        error.response.success = false
-        if (error.response.status != HTTP_UNPROCESSABLE_ENTITY) {
-            store.commit('error/setCode', error.response.status, { root: true })
-        }
-        return error.response
-    }
-)
-
-//共通リクエスト処理
-httpClient.interceptors.request.use((request) => {
-    console.debug(request)
-    return request
-})
-
-export default store
+Vue.use(VueCompositionApi)
+window.createApp = createApp
---
+++ resources/js/components/firstview.vue
@@ -0,0 +1,26 @@
+<template>
+    <div></div>
+</template>
+<script lang="ts">
+import { defineComponent, getCurrentInstance } from '@vue/composition-api'
+
+export default defineComponent({
+    name: 'firstview',
+    setup() {
+        const instance = getCurrentInstance()
+        const axios = instance!.proxy.$axios
+
+        const postText = async () => {
+            try {
+                const res = await axios.post(`/api/3/memos`, 'test request')
+                console.log('API success', res.data)
+                return true
+            } catch (error) {
+                alert('作成に失敗しました。')
+                console.log('API error', error)
+                return false
+            }
+        }
+    },
+})
+</script>
